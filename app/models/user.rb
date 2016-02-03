d2476d8849ccd0b5842f3d9075b1dca89c2a3bf2
Alvaro I. Mangual <amangual@live.com>
2016-02-03 09:56:27 -0600

    Preparing for user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :wikis
+  mount_uploader :avatar, AvatarUploader
 
   after_initialize :default_role
          
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201221039) do
+ActiveRecord::Schema.define(version: 20160203023619) do
 
   create_table "collaborators", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160201221039) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
